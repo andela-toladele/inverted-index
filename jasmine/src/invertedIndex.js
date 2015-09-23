ac9bc29c7d12bbfc74a33011f139a483e9bfc611
andela-toladele <toyindipol@gmail.com>
2015-09-23 17:31:39 +0100

    Doc - add comment

--- jasmine/src/invertedIndex.js
+++ jasmine/src/invertedIndex.js
@@ -1,7 +1,9 @@
 var Index = function() {};
 
+//This object holds generated index
 Index.prototype.generatedIndex = {};
 
+//Reads and returns a json file from the specified path 
 Index.prototype.readJsonFile = function(filePath) {
 
   var jsonFile;
@@ -17,13 +19,16 @@ Index.prototype.readJsonFile = function(filePath) {
   return jsonFile;
 }
 
+//Creates index from the specified json file
 Index.prototype.createIndex = function(filePath) {
 
+  //Retrieves the texts from the json file
   var allTexts = this.readJsonFile(filePath);
 
-  var posIndex = [];
-  var uniqueTexts = [];
+  var posIndex = []; //Stores the index
+  var uniqueTexts = []; //Stores unique texts
 
+  //Loops through the entire text array and generates index for text
   for (var i = 0; i < allTexts.length; i++) {
 
     var text = allTexts[i].text.replace(".", "").split(" ");
@@ -45,17 +50,21 @@ Index.prototype.createIndex = function(filePath) {
       }
     }
   }
+  //Creates the index object
   this.generatedIndex = {
     uniqueTexts: uniqueTexts,
     textIndex: posIndex
   };
 }
 
+//Returns the generated index
 Index.prototype.getIndex = function() {
 
   return this.generatedIndex;
 }
 
+//Searches for the index of the specified item in the uniqueTexts array of this.generatedIndex 
+//object and returns the matching index from the textIndex array of this.generatedIndex object
 Index.prototype.searchIndex = function(item) {
 
   var itemIndex = this.getIndex().uniqueTexts.indexOf(item);
